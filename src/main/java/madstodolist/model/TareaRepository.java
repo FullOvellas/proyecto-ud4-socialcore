10f7d4b3d9b8e4a2a720965962ef6bd611e4976f
margb7 <mariogonzalezgoian@gmail.com>
2023-01-26 13:10:04 +0100

    chore: estructura inicial del proyecto

--- src/main/java/madstodolist/model/TareaRepository.java
+++
@@ -1,6 +0,0 @@
-package madstodolist.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TareaRepository extends CrudRepository<Tarea, Long> {
-}
--- src/main/java/madstodolist/model/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package madstodolist.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String s);
-}
