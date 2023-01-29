3c993b61247f0931e543a52ce62dd64e3c137fd7
FullOvellas <sergiotui1c@gmail.com>
2023-01-29 15:30:01 +0100

    WIP CRUD opperations

--- src/main/java/com/aad/proyectoud4socialcore/controller/UserController.java
+++ src/main/java/com/aad/proyectoud4socialcore/controller/UserController.java
@@ -2,11 +2,10 @@ package com.aad.proyectoud4socialcore.controller;
 
 import com.aad.proyectoud4socialcore.model.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -14,13 +13,21 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/api/user")
+    @GetMapping("/user")
     public ResponseEntity<?> getUser(@RequestParam(required = false) Long id) {
 
         if (id == null)
             return ResponseEntity.ok().body(userRepository.findAll());
 
         return ResponseEntity.ok().body(userRepository.findById(id));
+
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable long id) {
+
+        userRepository.deleteById(id);
+        return ResponseEntity.ok().build();
 
     }
 
