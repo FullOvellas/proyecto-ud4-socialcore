fdacf214e20402869b59c0eec39f6f4200ec84b4
FullOvellas <sergiotui1c@gmail.com>
2023-01-31 20:06:07 +0100

    fix MeetingRepository

--- src/main/java/com/aad/proyectoud4socialcore/model/repository/MeetingRepository.java
+++ src/main/java/com/aad/proyectoud4socialcore/model/repository/MeetingRepository.java
@@ -3,5 +3,5 @@ package com.aad.proyectoud4socialcore.model.repository;
 import com.aad.proyectoud4socialcore.model.entity.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MeetingRepository implements JpaRepository<Meeting, Long> {
+public interface MeetingRepository  JpaRepository<Meeting, Long> {
 }
