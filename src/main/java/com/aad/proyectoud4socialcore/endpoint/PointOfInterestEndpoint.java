b4572aeff137283b1792c30eeddaceec5c4138dc
FullOvellas <sergiotui1c@gmail.com>
2023-01-31 18:47:01 +0100

    feat(endpoints): basic endpoints

---
+++ src/main/java/com/aad/proyectoud4socialcore/endpoint/PointOfInterestEndpoint.java
@@ -0,0 +1,29 @@
+package com.aad.proyectoud4socialcore.endpoint;
+
+import com.aad.proyectoud4socialcore.model.entity.PointOfInterest;
+import com.aad.proyectoud4socialcore.model.entity.User;
+import com.aad.proyectoud4socialcore.model.repository.PointOfInterestRepository;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.Endpoint;
+
+import java.util.List;
+
+@Endpoint
+@AnonymousAllowed
+public class PointOfInterestEndpoint {
+
+    private PointOfInterestRepository repository;
+
+    public PointOfInterestEndpoint(PointOfInterestRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<PointOfInterest> findAll() {
+        return repository.findAll();
+    }
+
+    public PointOfInterest add(PointOfInterest pointOfInterest) {
+        return repository.save(pointOfInterest);
+    }
+
+}
---
+++ src/main/java/com/aad/proyectoud4socialcore/model/repository/MeetingRepository.java
@@ -0,0 +1,7 @@
+package com.aad.proyectoud4socialcore.model.repository;
+
+import com.aad.proyectoud4socialcore.model.entity.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingRepository implements JpaRepository<Meeting, Long> {
+}
