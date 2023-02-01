647f0d37095723f5bf1cdf78a8c1de4cc59ac160
margb7 <mariogonzalezgoian@gmail.com>
2023-02-01 18:25:56 +0100

    feat: excepción para cuando ya existe un usuario a la hora de hacer un registro

---
+++ src/main/java/com/aad/proyectoud4socialcore/exception/UserAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.aad.proyectoud4socialcore.exception;
+
+public class UserAlreadyExistsException extends Exception {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
