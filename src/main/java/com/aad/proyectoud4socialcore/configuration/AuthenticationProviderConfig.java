337238c0191b3fa563bc11e6faa7adfaf32cfe0d
margb7 <mariogonzalezgoian@gmail.com>
2023-02-01 19:47:53 +0100

    feat: configuraciones de seguridad para usar encriptación de contraseñas

--- src/main/java/com/aad/proyectoud4socialcore/configuration/AuthenticationProviderConfig.java
+++ src/main/java/com/aad/proyectoud4socialcore/configuration/AuthenticationProviderConfig.java
@@ -2,9 +2,15 @@ package com.aad.proyectoud4socialcore.configuration;
 
 import com.aad.proyectoud4socialcore.service.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 public class AuthenticationProviderConfig extends WebSecurityConfigurerAdapter {
 
@@ -15,8 +21,25 @@ public class AuthenticationProviderConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         // Se establece el servicio custom de login de usuario en vez del por defecto de spring
-        auth.userDetailsService(userLoginService);
+        auth.userDetailsService(userLoginService).passwordEncoder(encoder());
 
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+
+        String hierarchy = "ROLE_ADMIN > ROLE_USER";
+
+        roleHierarchy.setHierarchy(hierarchy);
+
+        return roleHierarchy;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
