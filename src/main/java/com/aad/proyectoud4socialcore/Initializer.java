eaaac68db486434879fa007ef95b50a9deee9bdf
margb7 <mariogonzalezgoian@gmail.com>
2023-02-01 19:48:25 +0100

    feat: inicialización de datos modificada + creación de roles de usuario al iniciar la aplicación

--- src/main/java/com/aad/proyectoud4socialcore/Initializer.java
+++ src/main/java/com/aad/proyectoud4socialcore/Initializer.java
@@ -1,16 +1,29 @@
 package com.aad.proyectoud4socialcore;
 
+import com.aad.proyectoud4socialcore.model.entity.Role;
 import com.aad.proyectoud4socialcore.model.entity.SocialUser;
+import com.aad.proyectoud4socialcore.model.repository.RoleRepository;
 import com.aad.proyectoud4socialcore.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Component
 public class Initializer implements CommandLineRunner {
 
+    @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
 
     public Initializer(UserRepository userRepository) {
 
@@ -22,9 +35,20 @@ public class Initializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Stream.of("lucascabaleiro", "margb7", "FullOvellas")
-                .forEach(name -> userRepository.save(new SocialUser(name)));
+                .forEach(name -> {
 
-        userRepository.findAll().forEach(System.out::println);
+                    SocialUser user = new SocialUser();
+                    List<Role> roles = new ArrayList<>();
+
+                    roles.add(roleRepository.findByName("ROLE_ADMIN"));
+
+                    user.setEmail(name);
+                    user.setFullName(name);
+                    user.setPassword(encoder.encode("1234"));
+                    user.setRoles(roles);
+
+                    userRepository.save(user);
+                });
 
     }
 }
