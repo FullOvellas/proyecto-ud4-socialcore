4d234d27200e4e4bea420439d61751ad5bd680d3
margb7 <mariogonzalezgoian@gmail.com>
2023-02-01 19:47:04 +0100

    feat: servicio de registro de usuario

--- src/main/java/com/aad/proyectoud4socialcore/service/UserRegisterService.java
+++ src/main/java/com/aad/proyectoud4socialcore/service/UserRegisterService.java
@@ -2,9 +2,12 @@ package com.aad.proyectoud4socialcore.service;
 
 import com.aad.proyectoud4socialcore.exception.UserAlreadyExistsException;
 import com.aad.proyectoud4socialcore.model.dto.UserDTO;
+import com.aad.proyectoud4socialcore.model.entity.Role;
 import com.aad.proyectoud4socialcore.model.entity.SocialUser;
+import com.aad.proyectoud4socialcore.model.repository.RoleRepository;
 import com.aad.proyectoud4socialcore.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,7 +18,13 @@ import java.util.ArrayList;
 public class UserRegisterService {
 
     @Autowired
-    public UserRepository userRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public SocialUser registerNewUserAccount(UserDTO userDTO) throws UserAlreadyExistsException {
 
@@ -26,14 +35,14 @@ public class UserRegisterService {
         }
 
         SocialUser user = new SocialUser();
-        ArrayList<String> roles = new ArrayList<>();
+        ArrayList<Role> roles = new ArrayList<>();
 
         // TODO: inlcuír dentro de un enum con los posibles roles de usuario
-        roles.add("NORMAL_USER");
+        roles.add(roleRepository.findByName("ROLE_USER"));
 
         user.setEmail(userDTO.getEmail());
         user.setFullName(userDTO.getFullName());
-        user.setPassword(userDTO.getPassword());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         user.setRoles(roles);
 
         return userRepository.save(user);
