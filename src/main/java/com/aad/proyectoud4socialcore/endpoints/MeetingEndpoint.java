63d6c25b7c6c313ce392d2660965fc32d755a81a
FullOvellas <sergiotui1c@gmail.com>
2023-02-03 11:11:03 +0100

    chore: removed field injection

---
+++ src/main/java/com/aad/proyectoud4socialcore/endpoints/MeetingEndpoint.java
@@ -0,0 +1,35 @@
+package com.aad.proyectoud4socialcore.endpoints;
+
+import com.aad.proyectoud4socialcore.model.entity.Meeting;
+import com.aad.proyectoud4socialcore.model.entity.SocialUser;
+import com.aad.proyectoud4socialcore.model.repository.MeetingRepository;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.Endpoint;
+
+import java.util.List;
+
+@Endpoint
+@AnonymousAllowed
+public class MeetingEndpoint {
+
+    private final MeetingRepository repository;
+
+    public MeetingEndpoint(MeetingRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Meeting> findAll() {
+        return repository.findAll();
+    }
+
+    public List<Meeting> findSocialUserMeetings(SocialUser user) {
+
+        return repository.findByParticipant(user);
+
+    }
+
+    public Meeting add(Meeting meeting) {
+        return repository.save(meeting);
+    }
+
+}
