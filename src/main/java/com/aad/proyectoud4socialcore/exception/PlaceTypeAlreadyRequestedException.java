1aea1b079ca17ac277a06bad3e0ea1aba30542ee
Sergio Alonso Pazo <sergiotui1c@gmail.com>
2023-02-06 14:38:57 +0100

    getNearbyPointsOfInterestForUser method

---
+++ src/main/java/com/aad/proyectoud4socialcore/exception/PlaceTypeAlreadyRequestedException.java
@@ -0,0 +1,9 @@
+package com.aad.proyectoud4socialcore.exception;
+
+public class PlaceTypeAlreadyRequestedException extends Exception {
+
+    public PlaceTypeAlreadyRequestedException(String message) {
+        super(message);
+    }
+
+}
