1dddf56a5cdfcaddc9232601d36252b4099a0f8f
margb7 <mariogonzalezgoian@gmail.com>
2023-02-06 13:48:40 +0100

    feat: excepción para cuando no se encuentra un usuario a partir de correo (por ej.)

---
+++ src/main/java/com/aad/proyectoud4socialcore/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.aad.proyectoud4socialcore.exception;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) {
+
+        super(message);
+
+    }
+
+}
