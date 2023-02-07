6901ff489b043e502e6cf363fd6e22f7052af368
Sergio Alonso Pazo <sergiotui1c@gmail.com>
2023-02-07 09:53:19 +0100

    calculate distance to point int residence

--- src/main/java/com/aad/proyectoud4socialcore/Initializer.java
+++ src/main/java/com/aad/proyectoud4socialcore/Initializer.java
@@ -1,9 +1,12 @@
 package com.aad.proyectoud4socialcore;
 
+import com.aad.proyectoud4socialcore.model.entity.PointOfInterest;
 import com.aad.proyectoud4socialcore.model.entity.Role;
 import com.aad.proyectoud4socialcore.model.entity.SocialUser;
 import com.aad.proyectoud4socialcore.model.repository.RoleRepository;
 import com.aad.proyectoud4socialcore.model.repository.UserRepository;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.OpeningHours;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -49,6 +52,11 @@ public class Initializer implements CommandLineRunner {
 
                     userRepository.save(user);
                 });
+
+        PointOfInterest point1 = new PointOfInterest("a", "a", new LatLng(42.2513809, -8.6900709), new OpeningHours(), "a", new ArrayList<>(), 1.0f);
+        PointOfInterest point2 = new PointOfInterest("a", "a", new LatLng(42.3407844, -8.6048713), new OpeningHours(), "a", new ArrayList<>(), 1.0f);
+
+        System.out.println(point1.calculateDistanceToPoint(point2.getCoordinates()));
 
     }
 }
