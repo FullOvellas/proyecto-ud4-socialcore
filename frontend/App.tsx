1c8dec82451f99610743e72dfa6476dd662c1155
margb7 <mariogonzalezgoian@gmail.com>
2023-02-13 14:36:00 +0100

    feat: valores temporales para probar info

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,6 @@
 import router from 'Frontend/routes.js';
 import { RouterProvider } from 'react-router-dom';
-import * as dotenv from 'dotenv';
 
 export default function App() {
-  dotenv.config()
   return <RouterProvider router={router} />;
 }
