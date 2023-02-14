098861f543e05f1acecd57ded491e4bcfa9a0316
margb7 <mariogonzalezgoian@gmail.com>
2023-02-14 18:45:22 +0100

    feat: endpoint para puntos de interés + método para buscar punto por ID

--- src/main/java/com/aad/proyectoud4socialcore/endpoints/PointOfInterestEndpoint.java
+++ src/main/java/com/aad/proyectoud4socialcore/endpoints/PointOfInterestEndpoint.java
@@ -1,7 +1,10 @@
 package com.aad.proyectoud4socialcore.endpoints;
 
+import com.aad.proyectoud4socialcore.exception.PointNotFoundException;
 import com.aad.proyectoud4socialcore.model.entity.PointOfInterest;
+import com.aad.proyectoud4socialcore.model.entity.SocialUser;
 import com.aad.proyectoud4socialcore.model.repository.PointOfInterestRepository;
+import com.aad.proyectoud4socialcore.service.UserAuthService;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 
@@ -12,9 +15,28 @@ import java.util.List;
 public class PointOfInterestEndpoint {
 
     private final PointOfInterestRepository repository;
+    private final UserAuthService userAuthService;
 
-    public PointOfInterestEndpoint(PointOfInterestRepository repository) {
+    public PointOfInterestEndpoint(PointOfInterestRepository repository, UserAuthService userAuthService) {
         this.repository = repository;
+        this.userAuthService = userAuthService;
+    }
+
+    /**
+     * Devuelve un punto de interés a partir de su Id
+     * @param id el id del punto
+     * @return el punto de interés
+     * @throws PointNotFoundException si no se encuentra el punto
+     */
+    public PointOfInterest getPoiFromId(long id) throws PointNotFoundException {
+
+        PointOfInterest point = repository.findPointOfInterestById(id);
+
+        if(point == null) {
+            throw new PointNotFoundException("Point not found");
+        }
+
+        return point;
     }
 
     public List<PointOfInterest> findAll() {
---
+++ src/main/java/com/aad/proyectoud4socialcore/exception/PointNotFoundException.java
@@ -0,0 +1,11 @@
+package com.aad.proyectoud4socialcore.exception;
+
+import dev.hilla.exception.EndpointException;
+
+public class PointNotFoundException extends EndpointException {
+
+    public PointNotFoundException(String message) {
+        super(message);
+    }
+
+}
