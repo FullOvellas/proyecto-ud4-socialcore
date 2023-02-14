78f77cc4be7b049de6977c2299ceb49eafd6ad68
margb7 <mariogonzalezgoian@gmail.com>
2023-02-14 09:19:32 +0100

    feat: stepper para registro

--- frontend/views/RegisterView.tsx
+++ frontend/views/RegisterView.tsx
@@ -13,27 +13,31 @@ import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {useEffect, useState} from "react";
 import {useSearchParams} from "react-router-dom";
-import {Step, StepLabel, Stepper} from "@mui/material";
-import {GoogleMap} from "@react-google-maps/api";
+import {ListItem, Step, StepButton, StepContent, StepLabel, Stepper} from "@mui/material";
+import {GoogleMap, useLoadScript} from "@react-google-maps/api";
+import Placeholder from "Frontend/components/placeholder/Placeholder";
+import {List} from "@mui/icons-material";
 
 export default function RegisterView() {
 
     const theme = createTheme();
 
+    const [email, setEmail] = useState<string>("");
+    const [fullName, setFullName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [passwordMatch, setPasswordMatch] = useState<string>("");
+
+    const [activeStep, setActiveStep] = useState<number>(0);
     const [error, setError] = useState<string>("")
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: "AIzaSyDenRxQ8_1INjqF9cvWTejzSrgo7lsHYtQ"
+    })
 
-        const data = new FormData(event.currentTarget);
+    function handleNext() {
 
-        const fullName = data.get('fullName')
-        const email = data.get('email');
-        const password = data.get('password');
-        const passwordRep = data.get('password_rep')
-
-        if (email == "" || fullName == "" || password == "" || passwordRep == "") {
+        if (email == "" || fullName == "" || password == "" || passwordMatch == "") {
             setError("Fields can't be empty")
             return
         }
@@ -43,7 +47,44 @@ export default function RegisterView() {
             return;
         }
 
-        if (password != passwordRep) {
+        if (password != passwordMatch) {
+            setError("Passwords don't match");
+            return;
+        }
+
+        if (password!.length < 5) {
+            setError("Password must have at least 5 characters");
+            return;
+        }
+
+        setError("");
+
+        setActiveStep(1);
+
+    }
+
+    function handlePrevious() {
+
+        setActiveStep(0);
+
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+
+        if (email == "" || fullName == "" || password == "" || passwordMatch == "") {
+            setError("Fields can't be empty")
+            return
+        }
+
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email!.toString())) {
+            setError("Email is not valid")
+            return;
+        }
+
+        if (password != passwordMatch) {
             setError("Passwords don't match");
             return;
         }
@@ -74,71 +115,23 @@ export default function RegisterView() {
 
     });
 
-    function FormStep() {
-        return (
-            <Box>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="fullName"
-                    label="Name"
-                    name="fullName"
-                    autoComplete="name"
-                    autoFocus
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password_rep"
-                    label="Repeat password"
-                    type="password"
-                    id="password_rep"
-                    autoComplete="current-password"
-                />
-                <Typography variant={"caption"} color={"rgb(155,155,155)"}>* Password must have
-                    at least 5 characters</Typography>
-                {error != "" &&
-                    <Typography color={"#FF0000"}>{error}</Typography>
-                }
-
-            </Box>
-        );
-    }
-
     function UserLocationStep() {
         return (
           <>
 
-              <GoogleMap/>
+              {!isLoaded &&
+                  <Placeholder/>
+              }
+
+              {isLoaded &&
+                  <GoogleMap/>
+              }
 
           </>
         );
     }
 
     const stepLabels = ["Credentials", "User location"];
-    const steps = [<FormStep/>, <UserLocationStep/>];
 
     return (
         <ThemeProvider theme={theme}>
@@ -167,35 +160,98 @@ export default function RegisterView() {
 
                             <Grid item xs={12} md={12}>
 
-                                <Box sx={{ width: '100%' }}>
+                                <form onSubmit={handleSubmit}>
 
-                                    <Stepper orientation="horizontal" activeStep={0}>
+                                    <Box sx={{ width: "80%", margin: "auto" }}>
 
-                                        {steps.map((value, index) => {
+                                        <Stepper dir={"ltr"} activeStep={activeStep} orientation={"vertical"}>
 
-                                                return (
-                                                    <Step key={"step_" + index} component="form" onSubmit={handleSubmit}>
+                                            <Step>
 
+                                                <StepLabel>{stepLabels[0]}</StepLabel>
 
-                                                        <StepLabel>{stepLabels[index]}</StepLabel>
+                                                <StepContent>
 
-                                                        {value}
+                                                    <ul>
+                                                        <TextField
+                                                            required
+                                                            value={fullName}
+                                                            onChange={event => setFullName(event.target.value)}
+                                                            id="fullName"
+                                                            label="Name"
+                                                            margin="normal"
+                                                            name="fullName"
+                                                            autoComplete="name"
+                                                        />
+                                                        <TextField
+                                                            required
+                                                            value={email}
+                                                            onChange={event => setEmail(event.target.value)}
+                                                            id="email"
+                                                            margin="normal"
+                                                            label="Email"
+                                                            name="email"
+                                                            autoComplete="email"
+                                                        />
+                                                        <TextField
+                                                            required
+                                                            value={password}
+                                                            onChange={event => setPassword(event.target.value)}
+                                                            name="password"
+                                                            margin="normal"
+                                                            label="Password"
+                                                            type="password"
+                                                            id="password"
+                                                            autoComplete="current-password"
+                                                        />
+                                                        <TextField
+                                                            margin="normal"
+                                                            required
+                                                            value={passwordMatch}
+                                                            onChange={event => setPasswordMatch(event.target.value)}
+                                                            name="password_rep"
+                                                            label="Repeat password"
+                                                            type="password"
+                                                            id="password_rep"
+                                                            autoComplete="current-password"
+                                                        />
 
-                                                    </Step>
-                                                );
+                                                        <Typography variant={"caption"} color={"rgb(155,155,155)"}>* Password must have
+                                                            at least 5 characters
+                                                        </Typography>
 
-                                            })
-                                        }
+                                                        {error != "" &&
+                                                            <Typography color={"#FF0000"}>{error}</Typography>
+                                                        }
 
-                                    </Stepper>
+                                                        <Button fullWidth={true} onClick={handleNext} variant={"contained"}>Next</Button>
 
-                                </Box>
+                                                    </ul>
+
+                                                </StepContent>
+
+                                            </Step>
+
+                                            <Step>
+
+                                                <StepLabel>{stepLabels[1]}</StepLabel>
+
+                                                <UserLocationStep/>
+
+                                            </Step>
+
+                                        </Stepper>
+
+                                    </Box>
+
+                                </form>
 
                             </Grid>
 
                             <Grid item md={12} xs={12}>
 
                                 <Button
+                                    disabled={true}
                                     type="submit"
                                     fullWidth
                                     variant="contained"
@@ -219,7 +275,9 @@ export default function RegisterView() {
                     </Container>
 
                 </Box>
+
             </Container>
+
         </ThemeProvider>
     );
 }
